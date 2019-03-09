e1d7ebbf77d5d55ac5f5313d1dfb5d0b04f3897e
Grimaldo <gqpsystem@gmail.com>
2019-03-09 15:43:13 -0500

    primer commit

---
+++ src/main/java/com/megacenter/MegaboticaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.megacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaboticaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaboticaBackendApplication.class, args);
+	}
+
+}
